e64134d5be8c77fd50b1cced58e8180f2568b8b0
Insha <ik3166016@gmail.com>
2025-05-10 15:07:26 +0500

    added model evaluation stage

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -11,15 +11,12 @@ import json
 
 # initialize dagshub
 import dagshub
-dagshub.init(repo_owner='himanshu1703', 
-             repo_name='swiggy-delivery-time-prediction', 
-             mlflow=True)
-
+dagshub.init(repo_owner='speedyskill', repo_name='swiggy-delivery-time-prediction', mlflow=True)
 # set the mlflow tracking server
-mlflow.set_tracking_uri("https://dagshub.com/himanshu1703/swiggy-delivery-time-prediction.mlflow")
+mlflow.set_tracking_uri("https://dagshub.com/speedyskill/swiggy-delivery-time-prediction.mlflow")
 
 # set mlflow experment name
-mlflow.set_experiment("DVC Pipeline")
+mlflow.set_experiment("Final Estimator")
 
 TARGET = "time_taken"
 
